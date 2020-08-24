8df4728eb2459d3aeb0f03219ddcdc3c8728983d
Smit Patel <smitp90333@gmail.com>
2020-08-24 11:59:04 +0530

    Init Commit

---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
+
+import web from "../src/images/1st.svg";
+import { NavLink } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <>
+      <h1>Welcome</h1>
+    </>
+  );
+}
+
+export default Home;
